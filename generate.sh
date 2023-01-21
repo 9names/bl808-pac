8e5ec92e3f5bf5b0dbbb7fd7104eb1a46d050205
9names <60134748+9names@users.noreply.github.com>
2023-01-21 20:02:09 +1100

    Update to newer svd2rust

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
-svd2rust -i bl808.svd --target riscv --const_generic --keep_list --nightly
+# use svd2rust 0.28.0 or later
+svd2rust -i bl808.svd --target riscv --const_generic --keep_list
 rm -rf src
 form -i lib.rs -o src/ 
 rm lib.rs
